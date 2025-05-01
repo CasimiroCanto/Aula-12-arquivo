e4a2f317f60134ca8e11599addcdff51718ada2f
CASIMIRO CAMARA CANTO <canto1951@gmail.com>
2025-05-01 08:04:33 -0300

    Add files via upload

---
+++ A12ex4a.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+char toLower(char c){
+    if (c >= 'A' && c <= 'Z') {
+        return c + 32;
+    }
+    return c;
+}
+
+int main() {
+    char palavra[6]; // 5 letras + '\0'
+    char letra;
+    int erros = 0;
+    int encontrou;
+    int letras_encontradas[5] = {0};
+    int total_letras = 0;
+    
+    printf("Digite uma palavra de até 5 letras: ");
+    scanf("%5s", palavra);
+    getchar(); 
+    
+    total_letras = strlen(palavra);
+    
+    while (erros < 3) {
+        encontrou = 0;
+        
+        printf("Digite uma letra para buscar na palavra: ");
+        scanf("%c", &letra);
+        getchar(); 
+        
+        for (int i = 0; i < total_letras; i++) {
+            if (toLower(palavra[i]) == letra && !letras_encontradas[i]) {
+                printf("Letra encontrada na posição %d!\n", i + 1);
+                letras_encontradas[i] = 1;
+                encontrou = 1;
+            }
+        }
+        
+        if (!encontrou) {
+            erros++;
+            printf("Letra não encontrada. Erros: %d/3\n", erros);
+        }
+        
+        
+               int todas_encontradas = 1;
+        for (int i = 0; i < total_letras; i++) {
+            if (!letras_encontradas[i]) {
+                todas_encontradas = 0;
+                break;
+            }
+        }
+        
+        if (todas_encontradas) {
+            printf("Parabéns! Você encontrou todas as letras da palavra!\n");
+            break;
+        }
+    }
+    
+    if (erros >= 3) {
+        printf("Você atingiu o limite de 3 erros. Fim do jogo.\n");
+    }
+    
+    printf("A palavra era: %s\n", palavra);
+    
+    return 0;
+}
